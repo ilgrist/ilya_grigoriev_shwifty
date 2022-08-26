7baaa25c06e9ceef33cb362fb0dde6934130a80e
Ilya Grigoriev Starr <ilgrist67@gmail.com>
2022-08-26 12:09:56 +0300

    final cleanup

--- src/services/util.service.js
+++ src/services/util.service.js
@@ -1,8 +1,6 @@
 export const utilService = {
   saveToStorage,
   loadFromStorage,
-  debounce,
-  makeId,
   getRandomColor,
 };
 
@@ -13,29 +11,6 @@ function saveToStorage(key, value) {
 function loadFromStorage(key) {
   let data = localStorage.getItem(key);
   return data ? JSON.parse(data) : undefined;
-}
-
-function debounce(func, wait = 500) {
-  let timeout;
-
-  return function executedFunction(...args) {
-    const later = () => {
-      clearTimeout(timeout);
-      func(...args);
-    };
-
-    clearTimeout(timeout);
-    timeout = setTimeout(later, wait);
-  };
-}
-
-function makeId(length = 10) {
-  var text = '';
-  var possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
-  for (var i = 0; i < length; i++) {
-    text += possible.charAt(Math.floor(Math.random() * possible.length));
-  }
-  return text;
 }
 
 function getRandomColor() {
