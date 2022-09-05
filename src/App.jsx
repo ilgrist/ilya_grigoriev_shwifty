709d45aa7b92b7a1000252b7ed89310582810249
Ilya Grigoriev Starr <ilgrist67@gmail.com>
2022-09-05 23:05:32 +0300

    removed router and inactive dependencies

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,14 @@
-import { HashRouter as Router, Route, Switch } from 'react-router-dom';
 import { AppHeader } from './cmps/AppHeader';
 import { Home } from './pages/Home';
 
 function App() {
   return (
-    <Router>
-      <div className="app">
-        <AppHeader />
-        <main className="main">
-          <Switch>
-            <Route path="/" component={Home}></Route>
-          </Switch>
-        </main>
-      </div>
-    </Router>
+    <div className="app">
+      <AppHeader />
+      <main className="main">
+        <Home />
+      </main>
+    </div>
   );
 }
 
